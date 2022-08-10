c39c8aeaa50511d0a4edbbda6853b57a307fd102
writehereyourname <writehereyouremail>
2022-08-10 10:30:59 +0200

    10.08

--- exercise.py
+++ exercise.py
@@ -1,3 +1,66 @@
 # do exercise here
 # upload to github for portfolio
 # thank me later when your git graph is as green as the python money u gonna earn
+
+
+#Task1
+print("```bash")
+
+a = int(234)
+print(a)
+
+b = float(43.12)
+print(b)
+
+c = complex(3, +2)
+print(c)
+
+d = str("Hallo")
+print(d)
+
+tis_is_true = True
+print(tis_is_true)
+print("```")
+
+
+#Task2
+print("```Bash")
+
+aa = 123
+print(aa, "is type of", type(aa))
+
+ab = 43.23
+print(ab, "is type of", type(ab))
+
+ac = (4-1J)
+print(ac, "is type of", type(ac))
+
+ad = str("How are you")
+print(ab, "is type of", type(ad))
+
+ae = True
+print(ae, "is type of", type(ae)) 
+
+print("```")
+
+#Task3
+
+print("```Bash")
+
+city = "Fürth"
+print(isinstance(city,int))
+print(isinstance(city,float))
+print(isinstance(city,bool))
+print(isinstance(city,complex))
+print(isinstance(city,str))
+
+print("```")
+
+#Task4
+
+print("```Bash")
+
+
+
+
+print("```")
